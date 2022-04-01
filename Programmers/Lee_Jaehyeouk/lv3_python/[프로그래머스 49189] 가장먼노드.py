eb068a059ffd164e6b2ad545e8bb916f380fcb9c
jaehyeouklee <leesea33@naver.com>
2022-04-02 01:38:21 +0900

    update prgrammers lv3

--- Programmers/Lee_Jaehyeouk/lv3_python/[프로그래머스 49189] 가장먼노드.py
+++ Programmers/Lee_Jaehyeouk/lv3_python/[프로그래머스 49189] 가장먼노드.py
@@ -1,30 +1,34 @@
 from collections import deque
 
-def bfs(v, visited, node):
+def bfs(v, distance, node):
     cnt = 0
     q = deque([[v, cnt]])
     while q:
         val = q.popleft()
         v = val[0]
         cnt = val[1]
-        if visited[v] == -1:
-            visited[v] = cnt
+        if distance[v] == -1:
+            distance[v] = cnt
             cnt += 1
             for e in node[v]:
                 q.append([e,cnt])
+#
 
 def solution(n,edge):
     answer = 0
-    visited = [-1] * (n+1)
+    distance = [-1] * (n+1)
     node = [[] for _ in range(n+1)]
     for e in edge:
         x = e[0]
         y = e[1]
         node[x].append(y)
         node[y].append(x)
-    bfs(1,visited,node)
-    for value in visited:
-        if value == max(visited):
+    bfs(1,distance,node)
+    print(distance)
+    print(node)
+    for value in distance:
+        if value == max(distance):
             answer = answer+1
     return answer
 
+solution(n,v)
