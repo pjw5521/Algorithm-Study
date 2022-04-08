cf31bbc383361b5bd7e881d07e2b4dc48487c15a
jaehyeouklee <leesea33@naver.com>
2022-04-08 18:10:40 +0900

    update Algorithm DP triangle Problem

--- Programmers/Lee_Jaehyeouk/DP/[프로그래머스 43105] 정수 삼각형.py
+++ Programmers/Lee_Jaehyeouk/DP/[프로그래머스 43105] 정수 삼각형.py
@@ -1,23 +1,24 @@
 def solution(triangle):
-    dp = [[0] * len(triangle) for _ in range(len(triangle))]
+    dp = []
+    for i in range(1,len(triangle)+1):
+        dp.append([0]*i)
     dp[0][0] = triangle[0][0]
+    print(dp)
 
-    # 점화식
-
-    for i in range(0, len(triangle) - 1):
-        for j in range(len(triangle[i])):
-            dp[i + 1][j] = max(dp[i + 1][j], dp[i][j] + triangle[i + 1][j])
-            dp[i + 1][j + 1] = max(dp[i + 1][j + 1], dp[i][j] + triangle[i + 1][j + 1])
-            #print(dp)
+    for i in range(1,len(dp)):
+        for j in range(len(dp[i])):
+            if j == 0:
+                dp[i][j] = triangle[i][j] +dp[i-1][j]
+            elif j == len(dp[i])-1:
+                print(triangle[i][j])
+                dp[i][j] = triangle[i][j] + dp[i-1][j-1]
+            else:
+                dp[i][j] = triangle[i][j] + max(dp[i-1][j-1],dp[i-1][j])
     return max(dp[-1])
 
 triangle = [[7], [3, 8], [8, 1, 0], [2, 7, 4, 4], [4, 5, 2, 6, 5]]
 
-x = [[1],[2,3],[4,5,6]]
-
-#print(solution(triangle))
-
-# answer = [ [0] * len(x) for _ in range(len(x))] 0 만드는 함수
+print(solution(triangle))
 
 
 
